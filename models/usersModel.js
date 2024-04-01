c3b8c689ef29367d772e8e8d7ba4292fe9e7815e
olicrea <olimar.digital@gmail.com>
2024-04-01 07:24:35 -0300

    operaciones CRUD en la clase: usersCrud

--- models/usersModel.js
+++ models/usersModel.js
@@ -21,52 +21,54 @@ const usersSchema = new mongoose.Schema({
 
 class usersCrud {
     constructor() {
-        this.model = mongoose.model('user', usersSchema);
+        this.model = mongoose.model('users', usersSchema); // O: 'users' es la colección en la db
     }
 
+    // O: métodos de instancia - https://mongoosejs.com/docs/guide.html#methods
     async listUsers(){
         const result = await this.model.find({});
         return result;
+        // O: Query.prototype.limit()
+        // O: query.limit(10);
+        // O: Un objeto de tipo Query en Mongoose representa una consulta que se puede ejecutar en la base de datos. No es el resultado de la consulta en sí, sino una representación de la consulta que se puede modificar o ejecutar más adelante.
     } 
 
-    async createUser(users){
-        const result = await this.model.create(users);
+    async createUser(user){
+        const result = await this.model.create(user);
+        console.log(result);
         return result;
     }
 
-    async getInformationUserId(){
-        const result = await this.model.findById(id);
+    async getInformationUserId(id){
+        const result = await this.model.findById({ _id: id });
         return result;
     }
 
-    async getInformationUserEmail(){
-        const result = await this.model.findOne(email);
+    async getInformationUserEmail(email){
+        const result = await this.model.findOne({ email: email });
         return result;
     }
 
-    async modifyUserId(){
-        const result = await this.model.findByIdAndUpdate(id);
+    async modifyUserId(id, update){
+        const result = await this.model.findByIdAndUpdate({ _id: id }, update);
         return result;
     }
 
-    async modifyUserEmail(){
-        const result = await this.model.findOneAndUpdate(email);
-        return result;    }
+    async modifyUserEmail(email, update){
+        const result = await this.model.findOneAndUpdate({ email: email }, update); 
+        // O: https://mongoosejs.com/docs/tutorials/findoneandupdate.html
+        return result;    
+    }
 
-    async deleteUserId(){
-        const result = await this.model.findByIdAndDelete(id);
-        return result;    }
+    async deleteUserId(id){
+        const result = await this.model.findByIdAndDelete({ _id: id });
+        return result;    
+    }
 
-    async deleteUserEmail(){
-        const result = await this.model.findOneAndDelete(email);
-        return result;    }
+    async deleteUserEmail(email){
+        const result = await this.model.findOneAndDelete({ email: email });
+        return result;    
+    }
 }
 
 module.exports = { usersCrud };
-
-// const show = async ()=>{
-//     const users = await usersModel.find();
-//     console.log(users);
-// }
-// show();
-    // const usersModel = mongoose.model('user', usersSchema);
