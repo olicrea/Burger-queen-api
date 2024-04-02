86b211dae106a8a9668fbeb40fc876ac98b0e7c6
olicrea <olimar.digital@gmail.com>
2024-04-02 10:58:59 -0300

    se agrega comentario para saber exactamente dónde están las variables de entorno

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+//Aquí van las variables de entorno (en mayúscula)
 exports.port = process.argv[2] || process.env.PORT || 8080;
 exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb://127.0.0.1:27017/burguer_queen';
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
