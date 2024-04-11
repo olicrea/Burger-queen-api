67cfb5e520c2f61b5f2613b84aa9408dfb1a71f3
olicrea <olimar.digital@gmail.com>
2024-04-11 09:40:09 -0400

    Se agrega CRUD, se borra comentarios en portugués, se implementan CRUD en las rutas respectivas

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,10 @@ const {
 
 const {
   getUsers,
+  postUsers,
+  putUsers,
+  deleteUsers,
+  getUsersBy,
 } = require('../controller/users');
 
 const usersCruded = usersCrud();
@@ -72,49 +76,19 @@ const initAdminUser = async (app, next) => {
  * (response).
  */
 
-/*
- * Português Brasileiro:
- *
- * Fluxo de uma aplicação e requisição em node - express:
- *
- * request  -> middleware1 -> middleware2 -> rota
- *                                             |
- * response <- middleware4 <- middleware3   <---
- *
- * A essência é que a requisição passa por cada uma das funções intermediárias
- * ou "middlewares" até chegar à função da rota; em seguida, essa função gera a
- * resposta, que passa novamente por outras funções intermediárias até finalmente
- * responder à usuária.
- *
- * Um exemplo de middleware poderia ser uma função que verifica se uma usuária
- * está realmente registrada na aplicação e tem permissões para usar a rota. Ou
- * também um middleware de tradução, que altera a resposta dependendo do idioma
- * da usuária.
- *
- * É por isso que sempre veremos os argumentos request, response e next em nossos
- * middlewares e rotas. Cada uma dessas funções terá a oportunidade de acessar a
- * requisição (request) e cuidar de enviar uma resposta (quebrando a cadeia) ou
- * delegar a requisição para a próxima função na cadeia (invocando next). Dessa
- * forma, a requisição (request) passa através das funções, assim como a resposta
- * (response).
- */
+
 
 module.exports = (app, next) => {
 
   app.get('/users', requireAdmin, getUsers);
 
-  app.get('/users/:uid', requireAuth, (req, resp) => {
-  });
+  app.get('/users/:uid', requireAuth, getUsersBy);
 
-  app.post('/users', requireAdmin, (req, resp, next) => {
-    // TODO: Implement the route to add new users
-  });
+  app.post('/users', requireAdmin, postUsers);
 
-  app.put('/users/:uid', requireAuth, (req, resp, next) => {
-  });
+  app.put('/users/:uid', requireAuth, putUsers);
 
-  app.delete('/users/:uid', requireAuth, (req, resp, next) => {
-  });
+  app.delete('/users/:uid', requireAuth, deleteUsers);
 
   initAdminUser(app, next);
 };
