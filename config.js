ed1b5adcd75849b1af84d2df293b92c692ddb98d
olicrea <olimar.digital@gmail.com>
2024-04-17 17:31:33 -0400

    se agregó vercel.json

--- config.js
+++ config.js
@@ -4,3 +4,7 @@ exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb://127.0.
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost';
 exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
+
+// exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb://127.0.0.1:27017/burguer_queen';
+// exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb+srv://olimongo:963852741.Mongoatlas@cluster0.7q8ducq.mongodb.net/';
+
